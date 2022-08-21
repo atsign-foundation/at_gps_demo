72ef90f9c825df69867261f5a40c6370af216fb5
Colin Constable <colin@constable.cc>
2022-08-21 00:42:33 -0700

    Update at_gps_demo.dart

--- bin/at_gps_demo.dart
+++ bin/at_gps_demo.dart
@@ -1,52 +1,56 @@
 import 'dart:convert';
-
-import 'package:at_gps_demo/at_gps_demo.dart' as at_gps_demo;
 import 'dart:io';
-
-List<int> data_buffer = [];
-List<int> datain_buffer = [];
 
 void main(List<String> arguments) async {
   int port = 2947;
+  List<int> dataBuffer = [];
 
   var socket = await Socket.connect('localhost', port);
   print('Client connected');
 
   try {
     socket.listen((List<int> event) {
-      bufferMe(event);
+      dataBuffer = bufferMe(event, dataBuffer);
     });
 
     var send = '?WATCH={"enable":true,"json":true};';
     socket.writeln(send);
   } on Exception catch (exception) {
-    print('Something bad happened on connection to server: ' +
-        exception.toString());
+    print('Unable to connect to server:  On port $port : $exception');
   } catch (error) {
-    print('Errors occur' + error.toString());
+    print('Errors occured: $error');
   }
 }
 
-void bufferMe(data) {
-  datain_buffer.addAll(data);
-  String query = utf8.decode(datain_buffer);
+List<int> bufferMe(data, List<int> dataBuffer) {
+  dataBuffer.addAll(data);
+  String query = utf8.decode(dataBuffer);
   var len = query.length;
-  if (query[len - 1] == '\n') {
-    try {
-      query = query.trimRight();
-      query = query.trimLeft();
-      var json = jsonDecode(query.toString());
-      //print(query);
-      Map send = {};
-      send['lat'] = (json['lat'].toString());
-      send['lon'] = (json['lon'].toString());
-      send['speed'] = (json['speed'].toString());
+        print('\n\r############$query\n\r###########');
+  if (!query.contains('"lat"') || !query.contains('"lon"')) {
+    // the first line just gives us a version number
+    // we will ignore and reset the buffer
+    dataBuffer.length = 0;
+  } else {
+    if (query[len - 1] == '\n') {
+      try {
+        query = query.trimRight();
+        query = query.trimLeft();
+        
+        var json = jsonDecode(query.toString());
+        Map send = {};
+        send['lat'] = (json['lat'].toString());
+        send['lon'] = (json['lon'].toString());
+        send['speed'] = (json['speed'].toString());
 
-      var sender = jsonEncode(send);
-      print(sender);
-    } catch (e) {
-      // print(e.toString());
+        var sender = jsonEncode(send);
+        print(sender);
+      } catch (e) {
+        print(e.toString());
+      }
+      // Reset the buffer
+      dataBuffer = [];
     }
   }
-  datain_buffer = [];
+  return (dataBuffer);
 }
