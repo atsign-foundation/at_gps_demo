85d63676c9223580e8dc7fa24bdb7dddf2d1a8a4
Colin Constable <colin@constable.cc>
2022-08-21 13:04:42 -0700

    send and receive yay

---
+++ bin/at_gps_receiver.dart
@@ -0,0 +1,240 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'dart:async';
+
+
+// external packages
+import 'package:args/args.dart';
+import 'package:logging/src/level.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+// @platform packages
+import 'package:at_client/at_client.dart';
+import 'package:at_utils/at_logger.dart';
+import 'package:at_onboarding_cli/at_onboarding_cli.dart';
+
+import 'package:at_gps_demo/home_directory.dart';
+import 'package:at_gps_demo/check_file_exists.dart';
+
+var pongCount = 0; // Pong counter
+var mqttSession = MqttServerClient('test.mosquitto.org', '');
+
+void main(List<String> args) async {
+        //starting secondary in a zone
+    var logger = AtSignLogger('atNautel reciever ');
+      runZonedGuarded(() async {
+        await snmpMqtt(args);
+      }, (error, stackTrace) {
+        logger.severe('Uncaught error: $error');
+        logger.severe(stackTrace.toString());
+      });
+    }
+
+
+Future <void> snmpMqtt(List<String> args) async {
+  InternetAddress sourceIp;
+  String mqttIP;
+  String mqttTopic;
+  String mqttUsername;
+  String nameSpace = 'ai6bh';
+  String deviceName;
+
+  InternetAddress target;
+  final AtSignLogger logger = AtSignLogger(' nautel ');
+  logger.hierarchicalLoggingEnabled = true;
+  logger.logger.level = Level.WARNING;
+
+  var parser = ArgParser();
+// Args
+  parser.addOption('key-file',
+      abbr: 'k', mandatory: false, help: 'transmitters atSign\'s atKeys file if not in ~/.atsign/keys/');
+  parser.addOption('receiver-atsign', abbr: 'r', mandatory: true, help: '@sign that recieves notifications');
+  parser.addOption('device-name', abbr: 'n', mandatory: true, help: 'Device name, used as AtKey:key');
+  parser.addOption('mqtt-host', abbr: 'm', mandatory: true, help: 'MQQT server hostname');
+  parser.addOption('mqtt-username', abbr: 'u', mandatory: true, help: 'MQQT server username');
+  parser.addOption('mqtt-topic', abbr: 't', mandatory: true, help: 'MQTT subjectname');
+  parser.addOption('source-ip-address',
+      abbr: 's', mandatory: false, defaultsTo: '0.0.0.0', help: 'Source IP address of SNMP');
+  parser.addFlag('verbose', abbr: 'v', help: 'More logging');
+
+  // Check the arguments
+  dynamic results;
+  String atsignFile;
+
+  String fromAtsign = 'unknown';
+  String? homeDirectory = getHomeDirectory();
+
+  try {
+    // Arg check
+    results = parser.parse(args);
+    // Find @sign key file
+    fromAtsign = results['receiver-atsign'];
+    mqttIP = results['mqtt-host'];
+    mqttUsername = results['mqtt-username'];
+    sourceIp = InternetAddress(results['source-ip-address']);
+    mqttTopic = results['mqtt-topic'];
+    deviceName = results['device-name'];
+    
+    var targetlist = await InternetAddress.lookup(mqttIP);
+    target = targetlist[0];
+
+    if (results['key-file'] != null) {
+      atsignFile = results['key-file'];
+    } else {
+      atsignFile = '${fromAtsign}_key.atKeys';
+      atsignFile = '$homeDirectory/.atsign/keys/$atsignFile';
+    }
+    // Check atKeyFile selected exists
+    if (!await fileExists(atsignFile)) {
+      throw ('\n Unable to find .atKeys file : $atsignFile');
+    }
+  } catch (e) {
+    print(parser.usage);
+    print(e);
+    exit(1);
+  }
+
+// Now on to the @platform startup
+  AtSignLogger.root_level = 'WARNING';
+  if (results['verbose']) {
+    logger.logger.level = Level.INFO;
+
+    AtSignLogger.root_level = 'INFO';
+  }
+
+  //onboarding preference builder can be used to set onboardingService parameters
+  AtOnboardingPreference atOnboardingConfig = AtOnboardingPreference()
+    //..qrCodePath = 'etc/qrcode_blueamateurbinding.png'
+    ..hiveStoragePath = '$homeDirectory/.$nameSpace/$fromAtsign/storage'
+    ..namespace = nameSpace
+    ..downloadPath = '$homeDirectory/.$nameSpace/files'
+    ..isLocalStoreRequired = true
+    ..commitLogPath = '$homeDirectory/.$nameSpace/$fromAtsign/storage/commitLog'
+    //..cramSecret = '<your cram secret>';
+    ..atKeysFilePath = atsignFile;
+
+  AtOnboardingService onboardingService = AtOnboardingServiceImpl(fromAtsign, atOnboardingConfig);
+
+  await onboardingService.authenticate();
+
+  // var atClient = await onboardingService.getAtClient();
+
+  AtClientManager atClientManager = AtClientManager.getInstance();
+
+  NotificationService notificationService = atClientManager.notificationService;
+
+// Keep an eye on connectivity and report failures if we see them
+  ConnectivityListener().subscribe().listen((isConnected) {
+    if (isConnected) {
+      logger.warning('connection available');
+    } else {
+      logger.warning('connection lost');
+    }
+  });
+
+//Waiting for sync breaks stuff for now
+// As we only use notifications thats just fine
+  // bool syncComplete = false;
+  // void onSyncDone(syncResult) {
+  //   _logger.info("syncResult.syncStatus: ${syncResult.syncStatus}");
+  //   _logger.info("syncResult.lastSyncedOn ${syncResult.lastSyncedOn}");
+  //   syncComplete = true;
+  // }
+
+  // // Wait for initial sync to complete
+  // _logger.info("Waiting for initial sync");
+  // syncComplete = false;
+  // atClientManager.syncService.sync(onDone: onSyncDone);
+  // while (!syncComplete) {
+  //   await Future.delayed(Duration(milliseconds: 100));
+  // }
+
+  // atClientManager.syncService.sync(onDone: () {
+  //   _logger.info('sync complete');
+  // });
+
+// Set up MQTT
+  mqttSession = MqttServerClient(mqttIP, deviceName, maxConnectionAttempts: 10);
+  final builder = MqttClientPayloadBuilder();
+
+  mqttSession.setProtocolV311();
+  mqttSession.keepAlivePeriod = 20;
+  mqttSession.autoReconnect = true;
+  // Pong Callback
+    void pong() {
+      logger.info('Mosquitto Ping response client callback invoked');
+  pongCount++;
+  }
+  mqttSession.pongCallback = pong;
+
+  // await mqttSession.connect(mqttUsername, 'KRYZ');
+  // print(mqttSession.connectionStatus);
+
+  /// Create a connection message to use or use the default one. The default one sets the
+  /// client identifier, any supplied username/password and clean session,
+  /// an example of a specific one below.
+  final connMess = MqttConnectMessage()
+      .withClientIdentifier('Mqtt_MyClientUniqueId')
+      // .withWillTopic('willtopic') // If you set this you must set a will message
+      // .withWillMessage('My Will message')
+      .startClean() // Non persistent session for testing
+      .authenticateAs(mqttUsername, '')
+      .withWillQos(MqttQos.atLeastOnce);
+  logger.info('Mosquitto client connecting....');
+  mqttSession.connectionMessage = connMess;
+
+  /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+  /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+  /// never send malformed messages.
+  try {
+    await mqttSession.connect();
+  } on NoConnectionException catch (e) {
+    // Raised by the client when connection fails.
+    logger.severe(' Mosquitto client exception - $e');
+    mqttSession.disconnect();
+  } on SocketException catch (e) {
+    // Raised by the socket layer
+    logger.severe(' Mosquitto socket exception - $e');
+    mqttSession.disconnect();
+  }
+
+  /// Check we are connected
+  if (mqttSession.connectionStatus!.state == MqttConnectionState.connected) {
+    logger.info(' Mosquitto client connected');
+  } else {
+    /// Use status here rather than state if you also want the broker return code.
+    logger
+        .severe(' Mosquitto client connection failed - disconnecting, status is ${mqttSession.connectionStatus}');
+    mqttSession.disconnect();
+    exit(-1);
+  }
+
+  notificationService.subscribe(regex: '$deviceName.$nameSpace@', shouldDecrypt: true).listen(((notification) async {
+    String keyAtsign = notification.key;
+    //Uint8List buffer;
+    keyAtsign = keyAtsign.replaceAll(notification.to + ':', '');
+    keyAtsign = keyAtsign.replaceAll('.' + nameSpace + notification.from, '');
+    if (keyAtsign == deviceName) {
+      logger.info('SNMP update recieved from ' + notification.from + ' notification id : ' + notification.id);
+      var json = notification.value!;
+      print(json);
+      await mqttSession.connect();
+
+      if (mqttSession.connectionStatus!.state == MqttConnectionState.connected) {
+        logger.info('Mosquitto client connected sending message');
+        mqttSession.publishMessage(mqttTopic, MqttQos.atMostOnce, builder.addString(json).payload!, retain: false);
+        builder.clear();
+      } else {
+        await mqttSession.connect();
+      }
+    }
+  }),
+      onError: (e) => logger.severe('Notification Failed:' + e.toString()),
+      onDone: () => logger.info('Notification listener stopped'));
+
+
+}
+
+
---
+++ bin/at_gps_send.dart
@@ -0,0 +1,211 @@
+import 'dart:convert';
+import 'dart:io';
+
+// external packages
+import 'package:args/args.dart';
+import 'package:logging/src/level.dart';
+// @platform packages
+import 'package:at_client/at_client.dart';
+import 'package:at_utils/at_logger.dart';
+import 'package:at_onboarding_cli/at_onboarding_cli.dart';
+// Local Packages
+import 'package:at_gps_demo/home_directory.dart';
+import 'package:at_gps_demo/check_file_exists.dart';
+
+void main(List<String> args) async {
+  int port = 2947;
+  List<int> dataBuffer = [];
+  final AtSignLogger logger = AtSignLogger(' gps send ');
+  logger.hierarchicalLoggingEnabled = true;
+  logger.logger.level = Level.SHOUT;
+
+  var parser = ArgParser();
+// Args
+  parser.addOption('key-file',
+      abbr: 'k',
+      mandatory: false,
+      help: 'This device\'s atSign\'s atKeys file if not in ~/.atsign/keys/');
+  parser.addOption('atsign', abbr: 'a', mandatory: true, help: 'Your atSign');
+  parser.addOption('toatsign',
+      abbr: 't', mandatory: true, help: 'Send data to this atSign');
+  parser.addOption('device-name', abbr: 'n', mandatory: true, help: 'Device name, used as AtKey:key');
+
+  parser.addFlag('verbose', abbr: 'v', help: 'More logging');
+
+
+// Check the arguments
+  dynamic results;
+  String atsignFile;
+  String fromAtsign = 'unknown';
+  String toAtsign = 'unknown';
+  String deviceName = 'unknown';
+  String? homeDirectory = getHomeDirectory();
+  String nameSpace = 'ai6bh';
+  String rootDomain = 'root.atsign.org';
+
+  try {
+    // Arg check
+    results = parser.parse(args);
+    // Find @sign key file
+    fromAtsign = results['atsign'];
+    toAtsign = results['toatsign'];
+    deviceName = results['device-name'];
+
+    if (results['key-file'] != null) {
+      atsignFile = results['key-file'];
+    } else {
+      atsignFile = '${fromAtsign}_key.atKeys';
+      atsignFile = '$homeDirectory/.atsign/keys/$atsignFile';
+    }
+    // Check atKeyFile selected exists
+    if (!await fileExists(atsignFile)) {
+      throw ('\n Unable to find .atKeys file : $atsignFile');
+    }
+  } catch (e) {
+    print(parser.usage);
+    print(e);
+    exit(1);
+  }
+
+// Now on to the @platform startup
+  AtSignLogger.root_level = 'SHOUT';
+  if (results['verbose']) {
+    logger.logger.level = Level.INFO;
+
+    AtSignLogger.root_level = 'INFO';
+  }
+
+  //onboarding preference builder can be used to set onboardingService parameters
+  AtOnboardingPreference atOnboardingConfig = AtOnboardingPreference()
+    ..hiveStoragePath = '$homeDirectory/.$nameSpace/$fromAtsign/storage'
+    ..namespace = nameSpace
+    ..downloadPath = '$homeDirectory/.$nameSpace/files'
+    ..isLocalStoreRequired = true
+    ..commitLogPath = '$homeDirectory/.$nameSpace/$fromAtsign/storage/commitLog'
+    ..rootDomain = rootDomain
+    ..atKeysFilePath = atsignFile;
+  AtOnboardingService onboardingService =
+      AtOnboardingServiceImpl(fromAtsign, atOnboardingConfig);
+  await onboardingService.authenticate();
+  AtClient? atClient = await onboardingService.getAtClient();
+  AtClientManager atClientManager = AtClientManager.getInstance();
+  NotificationService notificationService = atClientManager.notificationService;
+
+  bool syncComplete = false;
+  void onSyncDone(syncResult) {
+    logger.info("syncResult.syncStatus: ${syncResult.syncStatus}");
+    logger.info("syncResult.lastSyncedOn ${syncResult.lastSyncedOn}");
+    syncComplete = true;
+  }
+
+  // Wait for initial sync to complete
+  logger.info("Waiting for initial sync");
+  stdout.write("Syncing your data.");
+  syncComplete = false;
+  atClientManager.syncService.sync(onDone: onSyncDone);
+  while (!syncComplete) {
+    await Future.delayed(Duration(milliseconds: 500));
+    stderr.write(".");
+  }
+
+  var socket = await Socket.connect('localhost', port);
+  logger.info('Client connected');
+
+  try {
+    socket.listen((List<int> event) {
+      dataBuffer = bufferMe(event, dataBuffer, fromAtsign, toAtsign, nameSpace,deviceName,
+          notificationService, logger);
+    });
+
+    var send = '?WATCH={"enable":true,"json":true};';
+    socket.writeln(send);
+  } on Exception catch (exception) {
+    print('Unable to connect to server:  On port $port : $exception');
+  } catch (error) {
+    print('Errors occured: $error');
+  }
+}
+
+List<int> bufferMe(
+    data,
+    List<int> dataBuffer,
+    String fromAtsign,
+    String toAtsign,
+    String nameSpace,
+    String deviceName,
+    NotificationService notificationService,
+    AtSignLogger logger) {
+  dataBuffer.addAll(data);
+  String query = utf8.decode(dataBuffer);
+  if (!query.contains('"lat"') || !query.contains('"lon"')) {
+    // the first line just gives us a version number
+    // we will ignore and reset the buffer
+    dataBuffer.length = 0;
+  } else {
+    // We might get multiple lines in a data packet
+    // so let's split them
+    LineSplitter ls = LineSplitter();
+    List<String> lines = ls.convert(query);
+    for (var i = 0; i < lines.length; i++) {
+      // print('Line $i: ${lines[i]}');
+      query = lines[i];
+      try {
+        query = query.trimRight();
+        query = query.trimLeft();
+        var json = jsonDecode(query.toString());
+        Map send = {};
+        send['lat'] = (json['lat'].toString());
+        send['lon'] = (json['lon'].toString());
+        send['speed'] = (json['speed'].toString());
+
+        var sender = jsonEncode(send);
+        print(sender);
+        sendGps(fromAtsign, toAtsign, nameSpace, deviceName, notificationService, logger,
+            sender);
+      } catch (e) {
+        print(e.toString());
+      }
+      // Reset the buffer
+      dataBuffer.length = 0;
+    }
+  }
+  return (dataBuffer);
+}
+
+void sendGps(
+    String fromAtsign,
+    String toAtsign,
+    String nameSpace,
+    String deviceName,
+    NotificationService notificationService,
+    AtSignLogger logger,
+    String input) async {
+  var metaData = Metadata()
+    ..isPublic = false
+    ..isEncrypted = true
+    ..namespaceAware = true
+    ..ttr = -1
+    ..ttl = 10000;
+
+  var key = AtKey()
+    ..key = deviceName
+    ..sharedBy = fromAtsign
+    ..sharedWith = toAtsign
+    ..namespace = nameSpace
+    ..metadata = metaData;
+  if (!(input == "")) {
+    try {
+      await notificationService
+          .notify(NotificationParams.forUpdate(key, value: input),
+              onSuccess: (notification) {
+        logger.info('SUCCESS:$notification');
+      }, onError: (notification) {
+        logger.info('ERROR:$notification');
+      }, onSentToSecondary: (notification) {
+        logger.info('SENT:$notification');
+      }, waitForFinalDeliveryStatus: false);
+    } catch (e) {
+      logger.severe(e.toString());
+    }
+  }
+}
